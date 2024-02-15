95b72254737ebf13d89fff8df0b2c9f0dbb81828
Jovellmiranda <142292023+Jovellmiranda@users.noreply.github.com>
2024-02-15 11:54:19 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,7 @@ export default function Home() {
         <title> No Talent Required</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p> My name is Jovell Christian Q. Miranda. I am 21 years old.While  I don’t have a favorite or specific music, sometimes I prefer listening to OPM music because it makes me calm when I’m really stressed about doing something. Favorite Food: Much like seafood, Hobbies are more on playing games, watching movies and looking at things that I can have an interest in. 
-IT is my first choice. My parents supported me in choosing IT.
-I don’t know what I would be expecting in my future because time changes and anything can happen. I can still continue pursuing my IT more on databases.
-This subject is new to me because it is my first time taking the subject and some new Programming Languages. 
-</p>
-
+  
 <p>10 Things that require 0 talent:</p>
         <ol>
           <li>Being On Time</li>
